6e3ea6d4a15a5195f6e2ffbea02ea3744298ce12
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-08-18 13:51:05 +0900

    Create Alphago.py

---
+++ Alphago.py
@@ -0,0 +1,18 @@
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+# -3 과 3 사이에 1,000 개의 데이터 포인트를 만듭니다
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    # 1, 3, 9 이웃을 사용한 예측을 합니다
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+    ax.set_title(
+        "{} 이웃의 훈련 스코어: {:.2f} 테스트 스코어: {:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train), reg.score(X_test, y_test)))
+    ax.set_xlabel("특성")
+    ax.set_ylabel("타깃")
+axes[0].legend(["모델 예측", "훈련 데이터/타깃", "테스트 데이터/타깃"], loc="best")
+plt.show() 
