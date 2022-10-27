63df03e8f532baabbf48a39d5aabe64c7cd05eff
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-10-27 13:47:49 +0900

    Create anagonyeah.py

---
+++ anagonyeah.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+data = {
+  '이름' : ['쿠키', '판다', '펭귄', '사나', '나연', '모모'],
+  '특기' : ['춤', '노래', '예능', '물고기', '헤엄', '비행'],
+  '음식' : ['피자', '알파고', '문장', '아다곤예', '엘리너', '큐밥']
+}
+df = pd.DataFrame(data)
+df = pd.DataFrame(data, index=['1번', '2번', '3번', '4번', '5번', '6번'])
+df.index.name = 'num'
+print(df)
+df.to_csv('anagonye.csv')
+
+print('\n***********************\n')
+al = pd.read_csv('anagonye.csv')
+print(al)
