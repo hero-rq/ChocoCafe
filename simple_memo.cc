a13ff7e227628989422421d0c6c8a0a4b164eb07
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-03 17:35:47 +0900

    Create simple_memo.cc

---
+++ simple_memo.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+//#include <cstdio> //cstyle
+using namespace std;
+
+int main(){
+  printf("Hello panda\n");
+  cout << "Hello World!";
+  //printf("Hello, World!\n"); //cstyle
+
+  return 0;
+}
