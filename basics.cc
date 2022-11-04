afc6eff93d50b9dae883d7a63b2edf9bfcceebb1
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-04 16:26:36 +0900

    Update basics.cc

--- basics.cc
+++ basics.cc
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
+  printf("Hello Panda!\n");
   int a = 10;
   int b = 12;
-  
-  cout << a + b;
+
+  cout << "Hello Panda!\n";
+  cout << &a << "\n"<< &b;
 }
