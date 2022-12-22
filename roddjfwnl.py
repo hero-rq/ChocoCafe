2d9ba5038baf7f1233b7f33e2238132783d727ab
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-12-22 11:12:50 +0900

    Create roddjfwnl.py

---
+++ roddjfwnl.py
@@ -0,0 +1,15 @@
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+print("Setup Complete")
+
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
+
+# Call line below with no argument to check that you've loaded the data correctly
+step_1.check()
