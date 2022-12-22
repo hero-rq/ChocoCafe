f5fbc903f28d6d60a8a85e26d1a6822282acd1a2
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-12-22 11:15:42 +0900

    Update roddjfwnl.py

--- roddjfwnl.py
+++ roddjfwnl.py
@@ -11,5 +11,4 @@ iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 # Fill in the line below to read the file into a variable home_data
 home_data = pd.read_csv(iowa_file_path)
 
-# Call line below with no argument to check that you've loaded the data correctly
-step_1.check()
+print(home_data.describe())
